098f4dc4b3738b967211d59ffb0d4f90c7c347ba
GulfamASB <gulfamsaifi0298@gmail.com>
2024-04-07 20:08:25 +0530

    sfsdfd

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center bg-orange-400 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
-      <h1>GScoding</h1>
+      <h1>GSCoder</h1>
       </Link>
       <Link className="bg-white p-2" href={"/addTopic"}>
         Add The Post
