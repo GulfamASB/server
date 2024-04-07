0b396e3f5509c6703d4b8e082b8661448ca862c0
GulfamASB <gulfamsaifi0298@gmail.com>
2024-04-07 19:56:17 +0530

    dfdsdsf

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,75 +1,67 @@
-import React from 'react'
-import AddTopics from '@/components/Addtopic'
+"use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import baseUrl from '@/libs/baseUrl';
 
-export const metadata = {
-  manifest:'manifest.webmanifest',
-  title: {
-    template: '%s | Gulfam saifi || gulfaam saifi || gscoder',
-    default: 'Gulfam saifi || gulfaam saifi,gscoder', // a default is required when creating a template
-  },
-  description: 'Discover gscoder unequaled  design mastery at Gulfam saifi || gulfaam saifi , gulfam saifi web developer , gulfaam, ',
-  
-  metadataBase: new URL('https://gsserver.netlify.app'),
-  generator: ' Gulfam saifi || gulfaam saifi, gscoder',
-  applicationName: ' Gulfam saifi || gulfaam saifi, gscoder',
-  referrer: 'origin-when-cross-origin',
-  keywords: ['gulfaam saifi','coding', 'gulfaam','gccoder', 'gulfaam saifi','gulfam software engineer', 'gulfam web developer', 'gscoder','gs coder', 'gulfaam dadri', 'gulfam dadri','gulfam saifi dadri','gulfam developer'],
-  authors: [{ name: 'gulfam saifi || gulfaam saifi, gscoder', url:'https://gsserver.netlify.app' }, { name: 'gulfam saifi', url: 'https://gulfaam.com' }],
-  creator: 'gulfam saifi',
-  publisher: 'gulfam saifi || gulfaam saifi,gscoder ',
-  formatDetection: {
-    email: false,
-    address: false,
-    telephone: false,
-  },
-  icons: {
-    icon: [{ url: '/favicon.ico' }, new URL('/favicon.ico', 'https://gsserver.netlify.app')],
-    shortcut: ['/shortcut-icon.png'],
-    apple: [
-      { url: '/favicon.ico' },
-      { url: '/favicon.ico', sizes: '180x180', type: 'image/png' },
-    ],
-    other: [
-      {
-        rel: 'apple-touch-icon-precomposed',
-        url: '/favicon.ico',
-      },
-    ],
-  },
+export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-  verification: {
-    google: 'JKclvWmjV-7HYHecgGNwqJPGxx05JG_Xdq0Z66R-kE8',
-    other: {
-      me: ['my-email', 'my-link'],
-    },
-  },
+  const router = useRouter();
 
-  alternates: {
-    canonical: '/addTopic',
-  },
-  openGraph: {
-    title:'Gulfam saifi || gulfaam saifi || gscoder',
-    description: ' Gulfam saifi || gulfaam saifi , gscoder, Discover  unequaled  design mastery at Gulfam saifi || gulfaam saifi – where invention meets complication. Elevate your space with bespoke innards that  review luxury and reflect your unique style. aplusstudio noida',
-  
-    images: [{
-      url: '/gulfam.jpeg',
-      width: 400,
-      height: 300,
-    }]
-  },
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    if (!title || !description) {
+      alert("Title and description are required.");
+      return;
+    }
 
-}
+    try {
+      const res = await fetch(`${baseUrl}/api/topics`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
 
+      if (res.ok) {
+          router.refresh();
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a topic");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-const AddTopicData = () => {
   return (
-    <div>
-      <AddTopics />
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Title"
+      />
 
-export default AddTopicData
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+      />
+
+      <button
+        type="submit"
+        className="bg-orange-600 font-bold text-white py-3 px-6 w-fit"
+      >
+        Add Topic
+      </button>
+    </form>
+  );
+}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center bg-orange-400 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
-        GScoding
+      <h1>GScoding</h1>
       </Link>
       <Link className="bg-white p-2" href={"/addTopic"}>
         Add The Post
