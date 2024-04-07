b3b5391fdc81c83073231be7359dcc88a3f00aab
GulfamASB <gulfamsaifi0298@gmail.com>
2024-04-07 15:46:49 +0530

    DfsdfS

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -1,21 +1,40 @@
 import connectMongoDB from "@/libs/mongodb";
 import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
+import Cors from 'cors';
+import initMiddleware from 'next-connect';
+
+// Initialize CORS middleware
+const cors = initMiddleware(
+  Cors({
+    origin: '*', // Allow requests from any origin
+    methods: ['GET', 'POST', 'DELETE'], // Define the allowed HTTP methods
+  })
+);
 
 export async function POST(request) {
+  // Enable CORS for the POST request
+  await cors(request, request.res);
+
   const { title, description } = await request.json();
   await connectMongoDB();
   await Topic.create({ title, description });
   return NextResponse.json({ message: "Topic Created" }, { status: 201 });
 }
 
-export async function GET() {
+export async function GET(request) {
+  // Enable CORS for the GET request
+  await cors(request, request.res);
+
   await connectMongoDB();
   const topics = await Topic.find();
   return NextResponse.json({ topics });
 }
 
 export async function DELETE(request) {
+  // Enable CORS for the DELETE request
+  await cors(request, request.res);
+
   const id = request.nextUrl.searchParams.get("id");
   await connectMongoDB();
   await Topic.findByIdAndDelete(id);
